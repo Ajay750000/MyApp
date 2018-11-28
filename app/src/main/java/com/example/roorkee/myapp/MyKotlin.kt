d189441b4efd736b22ba12e69af1c7453a39748d
ajay <ajay7500007540@gmail.com>
2018-11-28 21:28:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/roorkee/myapp/MyKotlin.kt
@@ -0,0 +1,17 @@
+package com.example.roorkee.myapp
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MyKotlin: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
+
+
